6f46985ef21e7587a5d1b519b53c0be422864109
kegcheck <65734212+kegcheck@users.noreply.github.com>
2023-11-18 09:34:32 -0600

    start level_2

--- Assets/Scripts/PersistentGameObjects.cs
+++ Assets/Scripts/PersistentGameObjects.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class PersistentGameObjects : MonoBehaviour
 {
     public GameObject playerPrefab;
+    public GameObject gameManagerPrefab;
+    public AudioManager amPrefab;
 
     void Awake()
     {
         GameObject[] playersInScene = GameObject.FindGameObjectsWithTag("Player");
+        GameObject[] gameManagerInScene = GameObject.FindGameObjectsWithTag("GM");
+        AudioManager audiomanagerInScene = FindObjectOfType<AudioManager>();
+
+        if(gameManagerInScene.Length == 0)
+        {
+            Instantiate(gameManagerPrefab);
+        }
+
+        if(audiomanagerInScene == null)
+        {
+            Instantiate(amPrefab);
+        }
 
         if(playersInScene.Length == 0)
         {
             Instantiate(playerPrefab);
         }
+
 
     }
 }
